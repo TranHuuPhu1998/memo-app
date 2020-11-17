a7b0f1b90b04f146096b6d34bf625cc4396ba307
TranHuuPhu1998 <tranhuuphu877@gmail.com>
2020-11-17 14:06:33 +0700

    Updates

---
+++ precache-manifest.3fb09f4b48b94878dec64a1ec079c657.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81d77af4a61c1be00ab14566fe7668d1",
+    "url": "/memo-app/index.html"
+  },
+  {
+    "revision": "3be6629991501c520d1e",
+    "url": "/memo-app/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "409de7afbbc0b9a0ff1c",
+    "url": "/memo-app/static/css/main.e1862432.chunk.css"
+  },
+  {
+    "revision": "3be6629991501c520d1e",
+    "url": "/memo-app/static/js/2.742bf93c.chunk.js"
+  },
+  {
+    "revision": "409de7afbbc0b9a0ff1c",
+    "url": "/memo-app/static/js/main.35c12b6e.chunk.js"
+  },
+  {
+    "revision": "404c0018583f18dd5d07",
+    "url": "/memo-app/static/js/runtime~main.ef8d8901.js"
+  }
+]);
