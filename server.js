a1d94ae5aeaa43de7453cd3e3e88f91a3dd39a96
TranHuuPhu1998 <tranhuuphu877@gmail.com>
2020-11-17 14:45:39 +0700

    commit 100

--- server.js
+++ server.js
@@ -31,12 +31,6 @@ app.use(logger('dev'))
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('client/build'))
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
-    })
-}
 
 // Router đăng ký trong file router
 app.use('/projects', projects);
@@ -45,6 +39,13 @@ app.use('/dataMemo', memos);
 app.use('/htrdelete', htrdeletes);
 app.use('/user', users);
 app.use('/api', jwtUsers);
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 
 //Catch 404 Errors and forwoed them to error handle
